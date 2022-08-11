376dbe03b3e70023135dc602fb44e9cbd35485f9
tyankatsu0105 <frips.ryilsufupe+dev@gmail.com>
2022-08-11 21:46:01 +0900

    vscode

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ ln -sf "$PWD/zprofile" "$HOME/.zprofile"
 ln -sf "$PWD/gitconfig" "$HOME/.gitconfig"
 ln -sf "$PWD/gitconfig.local" "$HOME/.gitconfig.local"
 ln -sf "$PWD/gitignore_global" "$HOME/.gitignore_global"
+
+cd vscode && sh install.sh
